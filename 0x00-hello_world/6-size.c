eb8e40866c456b5a8204853a162c5c6c51bce46c
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-03-10 07:04:57 -0800

    print sizes of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - function needs to return an integer at the end of an execution
+*Return: return 0 as success and 1 as fail
+*/
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of a int: %d byte(s)\n", sizeof(int));
+	printf("Size of long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
