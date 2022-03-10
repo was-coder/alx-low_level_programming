1010de6c738d4473f6c2cb816d069a3a8491cad1
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-03-10 05:51:10 -0800

    a function that use puts to print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - function needs to return integer at the end of execution
+*Return: return 0 as success and 1 as fail
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
