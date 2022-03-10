8c00299dcbf476173b27353bd66da9e624f2bc9a
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-03-10 06:18:52 -0800

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - function needs to return some integers after execution
+*Return: returns 0 for success and 1 for fail
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
