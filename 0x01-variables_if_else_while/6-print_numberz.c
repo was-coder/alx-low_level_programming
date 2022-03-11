df627896942d9867fea57bf1932e45c02918c5f1
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-03-11 07:57:01 -0800

    print numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: return 0 as success
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar("%i", num);
+		num++
+	}
+
+	putchar('\n');
+
+	return (0);
+}
