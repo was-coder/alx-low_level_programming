76bdf4a9c9ebb26b75283b2a682efe2c98669888
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-03-11 09:35:27 -0800

    print two combinations of numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: A program that prints all possible
+ * different combinations of two digits
+ * Return: return 0 as success
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '	0'; i < '10'; i++)
+	{
+		for (j = (i + 1); j < '10'; j++)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i != '8' || j != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
