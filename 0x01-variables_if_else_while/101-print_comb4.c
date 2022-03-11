3ed877fafa3187c1d9bdefe2f5be28267b6ceae1
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-03-11 10:31:18 -0800

    print three combination of numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: Number that proints three digit
+ * Return: return 0 as success
+ * */
+
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a < 1000; a++)
+	{
+		b /= 100;
+		c = (a / 10) % 10;
+		d %= 10;
+
+		if (b < c && c < d)
+		{
+			putchar(b + '0');
+			putchar(c + '0');
+			putchar(d + '0');
+
+			if (a < 700)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
