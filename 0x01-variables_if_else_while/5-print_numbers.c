307ff572519bb51c871678f6f50fd33520d64f71
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-03-11 07:01:18 -0800

    print reminders of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: return 0 as success
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	do {
+		printf("%i\n", num);
+		num++;
+	} while (num < 10);
+
+	return (0);
+}
