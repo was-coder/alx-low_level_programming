ea27865c264abfcfd586ba097838d380bd996f87
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-03-16 15:52:30 -0700

    print every minute in a day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute in a day
+ * Return: return 0 as success
+ */
+
+void jack_bauer(void)
+{
+	int hours = 0;
+	int min = 0;
+
+	while (hours < 24)
+	{
+		while (min < 60)
+		{
+			_putchar('0' + (hours / 10));
+			_putchar('0' + (hours % 10));
+			_putchar(':');
+			_putchar('0' + (min / 10));
+			_putchar('0' + (min % 10));
+			min++;
+		}
+		min = 0;
+		hours++;
+	}
+}
