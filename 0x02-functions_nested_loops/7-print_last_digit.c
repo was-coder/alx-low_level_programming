1c77bada15cb0e0e753d4e9b4be575c50636d97f
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-03-16 15:45:41 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,13 +9,15 @@
 
 int print_last_digit(int n)
 {
+	int a;
+
 	if (n < 0)
-	{
-		n *= -1;
-	}
-	else
-	{
-		_putchar('0' + (n % 10));
-	}
-	return (n % 10);
+		n = -n;
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+	_putchar(a + '0');
+
+	return (a);
 }
