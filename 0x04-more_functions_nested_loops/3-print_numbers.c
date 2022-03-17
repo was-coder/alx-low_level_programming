0fde4f0fef08717d7606b8ff7d8e09a58739ff45
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-03-17 09:59:01 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers form 0 to 9
+ * Return: 0 as success
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+	return (0);
+}
