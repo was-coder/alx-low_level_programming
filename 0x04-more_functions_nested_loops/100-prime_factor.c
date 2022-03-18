dfcf5ba7c8c55beebbf6f59e8dcf44d6148edbfc
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-03-18 05:17:01 -0700

    print longeat prime number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - largest prime factors
+ * Return: 0 as success
+ */
+
+int main(void)
+{
+	long int i, f;
+
+	n = 612852475143;
+	for (f = 2; f <= i; f++)
+	{
+		if (i % f == 0)
+		{
+			i /= f;
+			f--;
+		}
+	}
+	printf("%ld\n", f);
+	return (0);
+}
