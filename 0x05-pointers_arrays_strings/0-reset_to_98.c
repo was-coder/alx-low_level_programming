30d213b744bbb0ffd07bc6a116b1f94d5841cd8b
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-03-21 11:19:35 -0700

    function that takes a pointer to an interger

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an integer parameter
+ * @n: parameter passed
+ *
+ * Return: 0 as success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
