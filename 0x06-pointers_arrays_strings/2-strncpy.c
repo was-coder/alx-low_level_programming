743db24a0fa632a12f554b7ace18b0b66d26c789
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-03-24 13:24:16 -0700

    copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * strncpy - copies a string
+ * @dest: destination
+ * @src: sources
+ * @n: amount of byte from src
+ *
+ * Return: 0 as success
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
