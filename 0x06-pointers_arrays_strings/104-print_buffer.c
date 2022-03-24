c65a9168318cd2f09d81162203983fe6680cb98f
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-03-24 14:15:57 -0700

    print buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -27,9 +27,9 @@ void print_buffer(char *b, int size)
 				if (k < size)
 					printf("%.2x", *(b + k));
 				else
-					printf (" ");
+					printf(" ");
 			}
-			printf (" ");
+			printf(" ");
 			for (l = j; l < j + 10; l++)
 			{
 				if (l >= size)
