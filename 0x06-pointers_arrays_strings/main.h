6f3ec360588ee5daaa8f8ccb4d6fb4fe8aa3c2f6
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-03-24 09:26:14 -0700

    feat:add a function to concatenate two string and all other prototypes

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -10,5 +7,3 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 int _putchar(char);
-
-#endif
