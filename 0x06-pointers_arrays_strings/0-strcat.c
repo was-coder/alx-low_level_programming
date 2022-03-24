995dbac9ad68f9abc1bf8f575522467f50f00dad
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-03-24 09:12:46 -0700

    feat:add a function to concatenate two string and all other prototypes

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,16 +10,12 @@
 
 char *_strcat(char *dest, char *src);
 {
-	int i;
-	int j;
-
-	i = 0;
-	j = 0;
+	int i = 0;
+	int j = 0;
 
 	while (dest[i] != '\0')
-	{
 		i++;
-	}
+
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
