35d7628873a91a36c7cdff21431dcc4a41125572
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-03-24 12:02:05 -0700

    feat:add function prototypes

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -7,3 +9,4 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 int _putchar(char);
+#endif
