1dc7b1cb67c6454b3b7d250a9d3326c459dfe786
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-03-28 10:24:36 -0700

    fix:gets the length of a prefix substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,31 +2,32 @@
 
 /**
  * _strspn - gets the length of a prefix substring
- * @s: initial segment
- * @accept: accepted bytes
+ * @s: initial parameter
+ * @accept: aceepted byte
  *
- * Return: The number of byte accepted
+ * Return: the number of accepted bytes
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i, j, bool;
+	unsigned int i, j, bool;
 
-for (i = 0; *(s + i) != '\0'; i++)
-{
-	bool = 1;
-	for (j = 0; *(accept + j) != '\0'; j++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		if (*(s + i) == *(accept + j))
+		bool = 1;
+
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			bool = 1;
-			break;
+			if (*(s + i) == *(accept + j))
+			{
+				bool = 0;
+				break;
+			}
 		}
+
+		if (bool == 1)
+			break;
 	}
 
-	if (bool == 1)
-		break;
-}
-
-return (i);
+	return (i);
 }
