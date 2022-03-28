ffe29d3ec44a6ffead8ebd81b341446ce5d7a96d
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-03-28 10:20:24 -0700

    fix:gets the length of a prefix substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,22 +10,23 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	for (i = 0; *(s + i) != '\0'; i++)
+unsigned int i, j, bool;
+
+for (i = 0; *(s + i) != '\0'; i++)
+{
+	bool = 1;
+	for (j = 0; *(accept + j) != '\0'; j++)
 	{
-		bool = 1;
-
-		for (j = 0; *(accept + j) != '\0'; j++)
+		if (*(s + i) == *(accept + j))
 		{
-			if (*(s + i) == *(accept + j))
-			{
-				bool = 0;
-				break;
-			}
-		}
-
-		if (bool == 1)
+			bool = 1;
 			break;
+		}
 	}
 
-	return (i);
+	if (bool == 1)
+		break;
+}
+
+return (i);
 }
