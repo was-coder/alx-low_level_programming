3910f9b16a51623426c941b492f726d23d9d03bc
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-03-28 10:31:44 -0700

    feat:finds the first occurrence of the substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strstr - finds the first occurrence of the substring
+ * @haystack: all string
+ * @needle: substring
+ *
+ * Return: to the beginning of substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *bhaystack;
+	char *pneedle;
+
+	while (*haystack != '\0')
+	{
+		bhaystack = haystack;
+		pneedle = needle;
+
+		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
+		{
+			haystack++;
+			needle++;
+		}
+
+		if (!*pneedle)
+			return (bhaystack);
+		haystack = bhaystack + 1;
+	}
+
+	return (0);
+}
