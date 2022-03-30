2170fa955d4e94c43587fa4a3829be7ffaad7c69
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-03-29 22:51:19 -0700

    function that prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string
+ * @s: parameter passed
+ *
+ * Return: 0 as success
+ */
+
+void _puts_recursion(char *s);
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
